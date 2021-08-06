82a828eb8a0ab00257033963712bab4c32ece5f0
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-08-06 11:52:29 +0900

    add

--- fizzbuzz/answer/answer1.php
+++ fizzbuzz/answer/answer1.php
@@ -1,8 +1,8 @@
 <?php
+
     for ($i; $i < 101; $i++) {
         // display $i
         echo $i;
         // start a new line
         echo "\n";
     }
-?>
---
+++ fizzbuzz/answer/answer2.php
@@ -0,0 +1,13 @@
+<?php
+    for ($i; $i <= 100; $i++) {
+        // echo ($i%3==0) ? "Fizz" : $i;
+        if ($i%3==0) {
+            echo "Fizz";
+            echo "\n";
+        } else {
+            echo $i;
+            echo "\n";
+        }
+    }
+?>
+
